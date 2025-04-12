0dbeede9978943025e531c4e6ff499b1fa1becb9
CodedByGrubba <167589691+CodedByGruba@users.noreply.github.com>
2025-04-12 21:37:11 +0200

    Add AddSecondCommand

--- src/main/java/de/codedbygruba/HypersBot.java
+++ src/main/java/de/codedbygruba/HypersBot.java
@@ -9,13 +9,28 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.util.EnumSet;
-
+import java.util.List;
 
 /**
  * @author CodedByGruba
  * @since 20.03.2025
  */
 public class HypersBot {
+    private static HypersBot INSTANCE;
+
+    public static HypersBot getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new HypersBot();
+        }
+        return INSTANCE;
+    }
+
+    public List<String> allowedRoles = List.of(
+            "1229182145235255421", "1229182187182751797",
+            "1309887484133638266", "1231949907892633632",
+            "1234966150756962385", "1355676921391747113"
+    );
+
     public static void main(String[] args) {
         try {
             JDA jda = JDABuilder.createDefault(SecretManager.getToken(),
